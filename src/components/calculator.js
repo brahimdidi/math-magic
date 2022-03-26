e515c429fc7c8b07076923b7096289fc4b2c75d2
brahim <brahimgb1team@gmail.com>
2022-03-26 13:11:43 +0000

    add events handler useState

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,43 +1,58 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
 class Calcul extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  constructor(prop) {
+    super(prop);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.handler = this.handler.bind(this);
+  }
+
+  handler(e) {
+    this.setState((state) => calculate(state, e.target.textContent));
   }
 
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="calculator">
-        <input type="number" className="input" placeholder="0" />
-        <ul className="ac">
-          <button type="button">ac</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button className="bg-orange" type="button">÷</button>
+        <div className="input">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <ul className="AC">
+          <button onClick={this.handler} type="button">AC</button>
+          <button onClick={this.handler} type="button">+/-</button>
+          <button onClick={this.handler} type="button">%</button>
+          <button onClick={this.handler} className="bg-orange" type="button">÷</button>
         </ul>
         <ul className="789x">
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button className="bg-orange button-x" type="button">x</button>
+          <button onClick={this.handler} type="button">7</button>
+          <button onClick={this.handler} type="button">8</button>
+          <button onClick={this.handler} type="button">9</button>
+          <button onClick={this.handler} className="bg-orange button-x" type="button">x</button>
         </ul>
         <ul className="456">
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button className="bg-orange" type="button">-</button>
+          <button onClick={this.handler} type="button">4</button>
+          <button onClick={this.handler} type="button">5</button>
+          <button onClick={this.handler} type="button">6</button>
+          <button onClick={this.handler} className="bg-orange" type="button">-</button>
         </ul>
         <ul className="123">
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button className="bg-orange" type="button">+</button>
+          <button onClick={this.handler} type="button">1</button>
+          <button onClick={this.handler} type="button">2</button>
+          <button onClick={this.handler} type="button">3</button>
+          <button onClick={this.handler} className="bg-orange" type="button">+</button>
         </ul>
         <ul className="0+">
-          <button className="button-0" type="button">0</button>
-          <button type="button">.</button>
-          <button className="bg-orange" type="button">=</button>
+          <button onClick={this.handler} className="button-0" type="button">0</button>
+          <button onClick={this.handler} type="button">.</button>
+          <button onClick={this.handler} className="bg-orange" type="button">=</button>
         </ul>
       </div>
     );
